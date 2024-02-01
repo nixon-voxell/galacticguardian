4758266264d4140ffcf61cc11c645c4b04b7306e
Nixon <voxell.technologies@gmail.com>
2024-02-01 18:52:38 +0800

    feat: spawn tile & buttons

--- Assets/Scripts/Ship/ShipBuilder.cs
+++ Assets/Scripts/Ship/ShipBuilder.cs
@@ -5,9 +5,12 @@ public class ShipBuilder : MonoBehaviour
 {
     [Tooltip("Number of tiles counting from the center towards the edge but not including the center tile.")]
     [SerializeField] private uint TilesFromCenter;
+    [SerializeField] private float TileSize = 1.0f;
 
     [SerializeField] private TileNode TileNodePrefab;
     [SerializeField] private TileNode[] TileNodes;
+
+    private InGameHud InGameHud;
 
     private int2[] indexOffsets = new int2[]
     {
@@ -21,14 +24,18 @@ public class ShipBuilder : MonoBehaviour
         new int2(0, 1),
     };
 
-    private void Awake()
+    private void Start()
     {
+        this.InGameHud = UiManager.Instance.GetUi<InGameHud>();
+
         int gridLength = (int)this.TilesFromCenter * 2 + 1;
         int tileCount = gridLength * gridLength;
 
+        float tileStart = -this.TileSize * (this.TilesFromCenter + 0.5f);
+
         this.TileNodes = new TileNode[tileCount];
 
-        // Create tile entities
+        // Create tiles
         for (int y = 0; y < gridLength; y++)
         {
             for (int x = 0; x < gridLength; x++)
@@ -36,16 +43,10 @@ public class ShipBuilder : MonoBehaviour
                 int2 index2D = new int2(x, y);
                 int flattenIndex = mathx.flatten_int2(index2D, gridLength);
 
-                TileNode tileNode = Object.Instantiate(this.TileNodePrefab, this.transform);
+                Vector3 position = new Vector3(tileStart + x * this.TileSize, tileStart + y * this.TileSize, 0.0f);
 
-                // // Setup parent child relationship
-                // shipGridChildren.Add(new Child { Value = tileEntity });
-                // commands.AddComponent<Parent>(tileEntity, new Parent { Value = shipGridEntity });
-
-                // // Add BuildTile
-                // commands.AddComponent<BuildTile>(tileEntity, BuildTile.Default);
-
-                // tileNodes[flattenIndex] = tileEntity;
+                TileNode tileNode = Object.Instantiate(this.TileNodePrefab, position, Quaternion.identity, this.transform);
+                this.InGameHud.CreateBuildBtn(position);
 
                 this.TileNodes[flattenIndex] = tileNode;
             }
--- Assets/Scripts/UserInterface/InGameHud.cs
+++ Assets/Scripts/UserInterface/InGameHud.cs
@@ -1,17 +1,34 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class InGameHud : UiMono
 {
     private Button m_TileBtn;
 
-    private void Start()
+    protected override void Awake()
     {
+        base.Awake();
+
         this.m_TileBtn = this.Root.Q<Button>("tile-btn");
+    }
 
-        // Button newBtn = new Button();
+    public Button CreateBuildBtn(Vector3 worldPosition)
+    {
+        Button buildBtn = new Button();
 
-        this.Root.Add(this.m_TileBtn);
-        UnityEngine.Debug.Log(this.m_TileBtn.style.left);
-        this.m_TileBtn.style.left = 100.0f;
+        foreach (string className in this.m_TileBtn.GetClasses())
+        {
+            buildBtn.AddToClassList(className);
+        }
+
+        Vector2 position = RuntimePanelUtils.CameraTransformWorldToPanel(
+            this.Root.panel, worldPosition, Camera.main
+        );
+        buildBtn.style.left = position.x;
+        buildBtn.style.top = position.y;
+
+        this.Root.Add(buildBtn);
+
+        return buildBtn;
     }
 }
