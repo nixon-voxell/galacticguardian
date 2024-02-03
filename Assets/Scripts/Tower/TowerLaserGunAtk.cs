3f77b4cea3e838a910104c686ed23c048e8b0035
Kudoshi <brendenguan@live.com>
2024-02-03 18:57:11 +0800

    fix: bullet appearance and tower bullet

--- Assets/Scripts/Tower/TowerLaserGunAtk.cs
+++ Assets/Scripts/Tower/TowerLaserGunAtk.cs
@@ -18,7 +18,7 @@ public class TowerLaserGunAtk : MonoBehaviour, ITower
     {
         if (m_Target != null && Time.time > m_NextAtkTime)
         {
-            BulletDefault bullet = GameManager.Instance.PoolManager.m_EnemyBullet.GetNextObject();
+            BulletDefault bullet = GameManager.Instance.PoolManager.m_TowerBullet.GetNextObject();
             bullet.transform.position = transform.position;
             bullet.transform.rotation = Util.LookAt2DRotation(transform.position, m_Target.transform.position);
             bullet.StartBullet(m_Tower.BulletStat);
