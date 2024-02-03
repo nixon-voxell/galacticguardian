c891d5c12332b14e5b405439be48ab27504862d4
Kudoshi <brendenguan@live.com>
2024-02-03 13:39:12 +0800

    cleanup: rename player health to tile health

--- Assets/Scripts/Ship/TileHealth.cs
+++ Assets/Scripts/Ship/TileHealth.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour, IDamageable
+public class TileHealth : MonoBehaviour, IDamageable
 {
     public float MaxHealth;
      
     private float m_CurrentHealth;
 
-    private void Start()
+    private void OnEnable()
     {
+        // Future purposes better to move it to a function to be called at the tile node handler
         m_CurrentHealth = MaxHealth;
     }
 
     public void OnDamage(Transform attacker, float damage)
     {
         m_CurrentHealth -= damage;
-        Debug.Log($"Health: {m_CurrentHealth} | Damage: {damage}");
+        Debug.Log($"Node: {gameObject.name} | Health: {m_CurrentHealth} | Damage: {damage}");
 
         if (m_CurrentHealth <= 0 )
         {
