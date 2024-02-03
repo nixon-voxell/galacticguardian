31a1a0a7135a917aba5f63d95fbc259184c71c1b
Diviyan20 <diviyanr@gmail.com>
2024-02-03 21:27:57 +0800

    feat: Add Particle Systems

--- Assets/Scripts/Essence/CollectEssence.cs
+++ Assets/Scripts/Essence/CollectEssence.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class CollectEssence : GameStat
 {
+    [SerializeField] private GameObject m_Explosion;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Essence"))
         {
             EssenceCount++;
             Debug.Log("Essence Count: " + EssenceCount);
+            if(m_Explosion != null)
+            {
+                Instantiate(m_Explosion, other.transform.position, Quaternion.identity);
+            }
             Destroy(other.gameObject);
             
         }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float MovementSpeed;
     private float m_speedX, m_speedY;
     private Rigidbody2D rb;
+    [SerializeField] private GameObject m_Explosion;
+    [SerializeField] private GameObject m_Player;
 
     void Start()
     {
@@ -19,5 +21,15 @@ public class PlayerMovement : MonoBehaviour
         m_speedX = Input.GetAxisRaw("Horizontal") * MovementSpeed;
         m_speedY = Input.GetAxisRaw("Vertical") * MovementSpeed;
         rb.velocity = new Vector2(m_speedX, m_speedY);
-    }
-}
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (m_Explosion != null) {
+                Instantiate(m_Explosion, transform.position, Quaternion.identity);
+                Destroy(m_Player);
+            }
+        }
+
+        }
+ }
+
