071ec9f342e4caf6a1e14a1e9238cb93f4539cc6
Nixon <voxell.technologies@gmail.com>
2024-02-12 21:53:13 +0800

    feat: enable dynamic tile size

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -5,8 +5,19 @@ public class CameraFollow : SingletonMono<CameraFollow>
     [HideInInspector] public Transform PlayerTransform;
 
     [SerializeField] private Vector3 m_Offset;
-    [SerializeField] private Vector2 m_MaterialOffset;
     [SerializeField] private SpriteRenderer m_Background;
+
+    private Material m_BgMaterial;
+    private Vector2 m_MaterialOffset;
+    private float m_PositionMultiplier;
+
+    private void Start()
+    {
+        this.m_BgMaterial = this.m_Background.material;
+        // Initial position is the offset
+        this.m_MaterialOffset = this.m_BgMaterial.GetVector("_Position");
+        this.m_PositionMultiplier = this.m_BgMaterial.GetFloat("_Size") * 0.5f;
+    }
 
     private void Update()
     {
@@ -18,14 +29,14 @@ public class CameraFollow : SingletonMono<CameraFollow>
         this.transform.position = this.PlayerTransform.position + this.m_Offset;
         if (m_Background != null)
         {
-            this.m_Background.material.SetVector(
+            this.m_BgMaterial.SetVector(
             "_Position",
             new Vector2(
                 this.transform.position.x,
                 this.transform.position.y
-            ) * 0.5f + this.m_MaterialOffset
+            ) * this.m_PositionMultiplier + this.m_MaterialOffset
         );
         }
-        
+
     }
 }
