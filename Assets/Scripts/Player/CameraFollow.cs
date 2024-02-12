3aec460e07e9c005bdb09eeaed5629ae7f265fcd
Kudoshi <brendenguan@live.com>
2024-02-12 21:49:01 +0800

    feat: Parallax effect

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -6,7 +6,21 @@ public class CameraFollow : SingletonMono<CameraFollow>
 
     [SerializeField] private Vector3 m_Offset;
     [SerializeField] private Vector2 m_MaterialOffset;
-    [SerializeField] private SpriteRenderer m_Background;
+    [SerializeField] private SpriteRenderer m_GridBackground;
+    [SerializeField] private Transform m_Background;
+    [SerializeField] private float m_BG1ParallaxFactor;
+    [SerializeField] private Transform m_Background2;
+    [SerializeField] private float m_BG2ParallaxFactor;   
+    [SerializeField] private Transform m_Background3;
+    [SerializeField] private float m_BG3ParallaxFactor;
+
+
+    private Vector2 m_PreviousBGPosition;
+    
+    private void Start()
+    {
+        m_PreviousBGPosition = transform.position;
+    }
 
     private void Update()
     {
@@ -16,16 +30,31 @@ public class CameraFollow : SingletonMono<CameraFollow>
         }
 
         this.transform.position = this.PlayerTransform.position + this.m_Offset;
-        if (m_Background != null)
+        if (m_GridBackground != null)
         {
-            this.m_Background.material.SetVector(
+            this.m_GridBackground.material.SetVector(
             "_Position",
             new Vector2(
                 this.transform.position.x,
                 this.transform.position.y
             ) * 0.5f + this.m_MaterialOffset
-        );
+            );
         }
+
+
+        // Background parallax
+
+        if (transform.position.x != m_PreviousBGPosition.x || transform.position.y != m_PreviousBGPosition.y)
+        {
+            Vector2 translatedAmt = m_PreviousBGPosition - (Vector2) transform.position;
+
+            m_Background.Translate(translatedAmt * m_BG1ParallaxFactor);
+            m_Background2.Translate(translatedAmt * m_BG2ParallaxFactor);
+            m_Background3.Translate(translatedAmt * m_BG3ParallaxFactor);
+
+            m_PreviousBGPosition = transform.position;
+        }
+
         
     }
 }
